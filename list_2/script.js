e38e67593b14b26a119b4fc6df39a57c067faf1c
Guilherme731 <gulherme_moreira6@estudante.sesisenai.org.br>
2025-03-21 09:28:42 -0300

    Exercise 19

--- list_2/script.js
+++ list_2/script.js
@@ -102,3 +102,12 @@ function ex18(){
 
     verificarAprovacao(parseFloat(prompt('Digite sua nota')));
 }
+
+//Exercício 19
+function ex19(){
+    for(let i = 0; i <= 30; i++){
+        if(i % 2 === 0){
+            console.log(i);
+        }
+    }
+}
